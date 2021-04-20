ba5dd9e9e2f7c98105a8813a63f9a824309ec330
ye-susan <susanye3@gmail.com>
2021-04-20 11:10:04 -0700

    add event handlers

--- src/components/input-box/input-box.jsx
+++ src/components/input-box/input-box.jsx
@@ -9,38 +9,51 @@ export const InputBox = () => {
     //pattern to match numbers, whitespace, and arithmetic operators (including single number without operator), excludes any alphabetical characters and expressions that end with operator
     const regex = /^\s*([-+]?)(\d+)(?:\s*([-+*/^])\s*((?:\s[-+])?\d+)\s*)+$|(^\s*([-+]?)(^\d+$))/;
 
+    // (^\s*([-+]?)(^\d+$))  -- individual number alone
+
     return regex.test(input);
   }
 
   const parseAndCalculate = () => {
-    //remove extra spaces
-    console.log("before: " + inputValue)
-    setInputValue(inputValue.replace(/\s{2,}/, ""));
-    console.log("after: " + inputValue)
-
-    //replace items inside () with answer
-  }
-  const handleChange = e => {
-    setInputValue(e.target.value); 
     
+
+    let total = 0;
+    let constantList = inputValue.split(/[/+*-]+/);
+    let operatorList = inputValue.split(/[\d\s]+/).filter((value) => {
+      return value !== "";
+    } );
+
+    console.log(constantList)
+    console.log(operatorList)
+    return total;
   }
 
-  const handleSubmit = e => {
-    e.preventDefault();
+
+  /************** EVENT HANDLERS ******************/
+  const handleChange = event => {
+    setInputValue(event.target.value); 
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
     if (inputValidationCheck(inputValue) !== true) {
-  
       console.log("Invalid Input: " + inputValue);
     } else {
       parseAndCalculate(inputValue)
     }
-    
   }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      handleSubmit(event);
+    }
+  }
+
   //if validation suceessful, then pass to history box -- use Context?
   //if input invalid, unhide text that warns of invalid input - and highlight box red
-
   return ( 
     <div className="input-box">
-      <input value={inputValue} onChange={handleChange} type="text"></input>
+      <input value={inputValue} onChange={handleChange} onKeyPress={handleKeyPress} type="text"></input>
       <button type="submit" onClick={handleSubmit}>Submit</button>
     </div>
   )
