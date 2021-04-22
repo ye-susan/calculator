eb2d77859407f026ba413037d07fd0e797c576f6
ye-susan <susanye3@gmail.com>
2021-04-22 00:27:54 -0700

    fixed logic for basic arithmetic calculations

--- src/components/input-box/input-box.jsx
+++ src/components/input-box/input-box.jsx
@@ -16,17 +16,16 @@ export const InputBox = () => {
 
   /************** PARSING AND CALCULATING ******************/
   const parseAndCalculate = () => {
-   
+
     //To follow the order of operations:
     //  Step 1: split on () --> evaluate and replace with answer
     //  Step 2: split on +/-  --> evaluate and replace with answer
-    //  Step 3: evaluate entire expression (which should only contain +/- left) and return answer
-
+    //  Step 3: evaluate entire expression (which should only contain +/-)
+    
     let value = inputValue.slice();
     //const multDivExp = /[+-]?\d*\.?\d+\s*([*/])\s*([+-]?\d*\.?\d+)/;
     const multDivExp = /[-]?\d*\.?\d+\s*([*/])\s*([-]?\d*\.?\d+)/;
     const addSubtExp = /([+-]?\d*\.?\d+)\s*([+-])\s*([+-]?\d*\.?\d+)/;
-
 
     //Step 1, evaluate items between parentheses (if they exist)
     while (value.includes("(") && value.includes(")")) {
@@ -44,7 +43,7 @@ export const InputBox = () => {
     //Step 2, evaluate expressions with * and / first (if they exist)
     while (value.includes("/") || value.includes("*")){
       //replace the * and / expressions with the evaluated answers
-      value = value.replace( multDivExp, function(exp) {
+      value = value.replace(multDivExp, exp => {
         return applyOperations(exp);
       })
       if(errorCheck(value)){
@@ -53,9 +52,10 @@ export const InputBox = () => {
       console.log(`value after calculating */ : ${value}`)
     }
 
-    while (value.includes("+") || value.includes("-")){
-      //replace the * and / expressions with the evaluated answers
-      value = value.replace( addSubtExp, function(exp) {
+    //While expression is not a standalone number/value, 
+    while (value.search(/^\s*([+-]?\d*\.?\d+)\s*$/) === -1) {
+      //replace the + and - expressions with the evaluated answers
+      value = value.replace(addSubtExp, exp => {
         return applyOperations(exp);
       })
       if(errorCheck(value)){
@@ -64,11 +64,6 @@ export const InputBox = () => {
       console.log(`value after calculating +- : ${value}`)
     }
     
-    if (errorCheck(value) === false) { 
-      value = applyOperations(value).toString();
-    }
-    //} //for while loop
-
     return errorCheck(value) ? setAnswerValue("Syntax Error") : setAnswerValue(`Answer: ${value}`);
   }
 
@@ -83,50 +78,58 @@ export const InputBox = () => {
 
   const applyOperations = (expression) => {
 
-    //check if number is negative
-    // if (expression[0] === "-" || expression[0] === "+") {
-    //   console.log(typeof(expression))
-    //   console.log("expression parsed: " + parseFloat(expression));
-    //   return parseFloat(expression);
-    // }
-
     //checking for double operators
-    //a is entire expression, b & c are the 1st and 2nd operator, and d is the constant/number
-    expression = expression.replace(/([+-])([+-])(\d|\.)/g, function (a, b, c, d) { 
-      return (b === c ? '+' : '-') + d; 
+    //exp is entire expression, op1 & op2 are the 1st and 2nd operator, and num is the constant/number
+    expression = expression.replace(/([+-])([+-])(\d|\.)/g, (exp, op1, op2, num) => { 
+      return (op1 === op2 ? '+' : '-') + num; 
     });
 
-    let operator = expression.split(/[\d\s]+/).filter( op => { 
+
+    //check if number is negative
+    let negative = 1;
+    // if (expression[0] === "-") {
+    //   console.log(`expression before slice ${expression}`)
+    //   expression = expression.slice(1);
+    //   console.log(`expression after slice ${expression}`)
+    //   negative = -1;
+    // }
+
+    let operator = expression.split(/([+-]?\d+)/g).filter( op => { 
       return op !== "" && op !== "." && op !== "(" && op !== ")" 
     });
 
-    let constants = expression.split(/[/+*-]+/); 
+    // let constants = expression.split(/^\s*([-]?\d*\.?\d+)/).filter( c => {
+    //   return c !== "";
+    // });
 
-    console.log("expression: " + expression);
-    console.log("operator: " + operator);
-    console.log("constants: " + constants)
-    console.log(`constants[0]: ${constants[0]} constants[1]: ${constants[1]}`);
+    // console.log("expression: " + expression);
+    // console.log("operator: " + operator);
+    // console.log("constants: " + constants)
+    // console.log(`constants[0]: ${constants[0]} constants[1]: ${constants[1]}`);
 
-    //for returning standalone "expressions"
-    if(!constants[1] || constants[1] === "") {
-      console.log(`returning parsed orig expression`)
-      return expression;
-    }
-    
-    switch(operator[0]) {
+    // //for returning standalone "expressions"
+    // if(!constants[1] || constants[1] === "") {
+    //   console.log(`returning parsed orig expression`)
+    //   return expression;
+    // }
+    let exp = /([+-]?\d*\.?\d+)\s*([*/+-])\s*([+-]?\d*\.?\d+)/;
+    let constants = exp.exec(expression);
+        //breaks up expression into 4 parts: for example if expression was "a+b"
+        // returns array: ["a+b", "a", "+", "b"]
+
+    switch(constants[2]) {
       case "/": 
-        return parseFloat(constants[0]) / parseFloat(constants[1]);
+        return parseFloat(constants[1]) / parseFloat(constants[3]);
       case "*": 
-        return parseFloat(constants[0]) * parseFloat(constants[1]);
+        return parseFloat(constants[1]) * parseFloat(constants[3]);
       case "-": 
-        return parseFloat(constants[0]) - parseFloat(constants[1]);
+        return parseFloat(constants[1]) - parseFloat(constants[3]);
       case "+": 
-        return parseFloat(constants[0]) + parseFloat(constants[1]); 
+        return parseFloat(constants[1]) + parseFloat(constants[3]); 
       default:
         return "Syntax Error";
     }
   }
-
 
 
 
