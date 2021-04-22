aaa203b57e3df9d5e664f5c949511f5b389c9161
ye-susan <susanye3@gmail.com>
2021-04-22 12:17:17 -0700

    refactor for dryness

--- src/components/input-box/input-box.jsx
+++ src/components/input-box/input-box.jsx
@@ -9,7 +9,7 @@ export const InputBox = () => {
   /************** INPUT VALIDATION ******************/
   const inputValidationCheck = input => {  
     // Output error message if input includes any alphabetical characters
-    const regex = /[^a-zA-Z]/;
+    const regex = /[a-zA-Z]/;
     return regex.test(input);
   }
 
@@ -23,63 +23,52 @@ export const InputBox = () => {
     //  Step 3: evaluate entire expression (which should only contain +/-)
     
     let value = inputValue.slice();
-    //const multDivExp = /[+-]?\d*\.?\d+\s*([*/])\s*([+-]?\d*\.?\d+)/;
     const multDivExp = /[-]?\d*\.?\d+\s*([*/])\s*([-]?\d*\.?\d+)/;
     const addSubtExp = /([+-]?\d*\.?\d+)\s*([+-])\s*([+-]?\d*\.?\d+)/;
+    const parenExp = /\(([^\)]+)\)/;
 
     //Step 1, evaluate items between parentheses (if they exist)
     while (value.includes("(") && value.includes(")")) {
-      //splitting based on items between parentheses
-      let newValue = value.replace(/\(([^\)]+)\)/, exp => {
-        exp = exp.replace("(","")
-        exp = exp.replace(")","")
-        return applyOperations(exp)
-      })
-      if (newValue == value) {
-        return setAnswerValue("Syntax Error")
-      } else {
-        value = newValue;
-      }
-      if(errorCheck(value)){
-        break;
+      value = solveAndCheck(value, parenExp);
+      if (value === "Syntax Error") { 
+        break; 
       }
     }
 
     //Step 2, evaluate expressions with * and / first (if they exist)
     while (value.includes("/") || value.includes("*")){
-      //replace the * and / expressions with the evaluated answers
-      let newValue = value.replace(multDivExp, exp => {
-        return applyOperations(exp);
-      })
-      if (newValue == value) {
-        return setAnswerValue("Syntax Error")
-      } else {
-        value = newValue;
+      value = solveAndCheck(value, multDivExp);
+      if (value === "Syntax Error") { 
+        break; 
       }
-      if(errorCheck(value)){
-        break;
-      }
-      console.log(`value after calculating */ : ${value}`)
     }
 
     //While expression is not a standalone number/value, 
     while (value.search(/^\s*([+-]?\d*\.?\d+)\s*$/) === -1) {
-      //replace the + and - expressions with the evaluated answers
-      let newValue = value.replace(addSubtExp, exp => {
-        return applyOperations(exp);
-      })
-      if (newValue == value) {
-        return setAnswerValue("Syntax Error")
-      } else {
-        value = newValue;
-      }
-
-      if(errorCheck(value)){
-        break;
+      value = solveAndCheck(value, addSubtExp);
+      if (value === "Syntax Error") { 
+        break; 
       }
     }
-    
-    return errorCheck(value) ? setAnswerValue("Syntax Error") : setAnswerValue(`Answer: ${value}`);
+
+    return setAnswerValue(`Answer: ${value}`);
+  }
+
+  const solveAndCheck = (value, regex) => {
+    //replace the operators +-/* with the evaluated answer
+    let newValue = value.replace( regex, exp => {
+      exp = exp.replace("(","")
+      exp = exp.replace(")","")
+      return applyOperations(exp);
+    })
+
+    if(newValue === value || errorCheck(newValue)) {
+      return "Syntax Error"
+    } else {
+      value = newValue;
+    }
+
+    return value;
   }
 
   const errorCheck = (value) => {
@@ -99,17 +88,11 @@ export const InputBox = () => {
       return (op1 === op2 ? '+' : '-') + num; 
     });
 
-
-    console.log("expression: " + expression);
-    
-    // console.log(`expItems[0]: ${expItems[0]} expItems[1]: ${expItems[1]}`);
-
-    //regex to isolate expressions, i.e. "3+3"
+    // regex to isolate expressions, i.e. "3+3"
     let expressionRegex = /([+-]?\d*\.?\d+)\s*([*/+-])\s*([+-]?\d*\.?\d+)/;
-    //breaks up expression into 4 parts: for example, if expression was "a+b"
-        // returns array: ["a+b", "a", "+", "b"]
+    // breaks up expression into 4 parts: for example, if expression was "a+b"
+    // returns array: ["a+b", "a", "+", "b"]
     let expItems = expressionRegex.exec(expression);
-    console.log("expItems: " + expItems)
         
     switch(expItems[2]) {
       case "/": 
@@ -134,8 +117,8 @@ export const InputBox = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    if (inputValidationCheck(inputValue) !== true) {
-      setAnswerValue(`Invalid Input: ${inputValue}`);
+    if (inputValidationCheck(inputValue)) {
+      setAnswerValue(`Invalid Input`);
     } else {
       parseAndCalculate(inputValue)
     }
