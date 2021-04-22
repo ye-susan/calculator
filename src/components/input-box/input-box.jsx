1891fb5dd3ee39aa7f3262d3639e7315822deca7
ye-susan <susanye3@gmail.com>
2021-04-21 18:19:51 -0700

    can calculate multiple constants

--- src/components/input-box/input-box.jsx
+++ src/components/input-box/input-box.jsx
@@ -7,12 +7,7 @@ export const InputBox = () => {
   const [answerValue, setAnswerValue] = useState("Answer: ");
   
   /************** INPUT VALIDATION ******************/
-  const inputValidationCheck = input => {
-    //pattern to match numbers, whitespace, and arithmetic operators (including single number without operator), excludes any alphabetical characters and expressions that end with operator
-    // const regex = /^\s*([-+]?)(\d+)(?:\s*([-+*/^])\s*((?:\s[-+])?\d+)\s*)+$|(^[-+]*\d+)/;
-
-    // (^\s*([-+]?)(^\d+$))  -- individual number alone
-    
+  const inputValidationCheck = input => {  
     // Output error message if input includes any alphabetical characters
     const regex = /[^a-zA-Z]/;
     return regex.test(input);
@@ -28,52 +23,91 @@ export const InputBox = () => {
     //  Step 3: evaluate entire expression and return answer
 
     let value = inputValue.slice();
+    //const multDivExp = /[+-]?\d*\.?\d+\s*([*/])\s*([+-]?\d*\.?\d+)/;
+    const multDivExp = /[-]?\d*\.?\d+\s*([*/])\s*([-]?\d*\.?\d+)/;
+    const addSubtExp = /([+-]?\d*\.?\d+)\s*([+-])\s*([+-]?\d*\.?\d+)/;
 
-    //Step 1, evaluate items between parentheses (if they exist)
-    if (value.includes("(")) {
-      let itemsInParen = value.split(/\(([^\)]+)\)/,).filter( i => {
-        return i !== "";
-      }); //splitting based on items between parentheses
-
-      //console.log(`itemsInParen after split: ${itemsInParen}`); 
-      let parenAnswer = 0;
-      for (let item of itemsInParen) {
-        // console.log(`item in parens: ${item}`)
-        item = item.replace(/[()]/,"");
-        parenAnswer = applyOperations(item);
-      }
+    //while value is not standalone number - keep evaluating
+    // while (value.search(/^\s*([+-]?\d*\.?\d+)\s*$/) === -1) {
       
-      value = value.replace(/\(([^\)]+)\)/,parenAnswer)
-      console.log(`itemsInParen: ${itemsInParen}`); 
-    }
+      //Step 1, evaluate items between parentheses (if they exist)
+      if (value.includes("(")) {
+        let itemsInParen = value.split(/\(([^\)]+)\)/).filter( i => {
+          return i !== "";
+        }); //splitting based on items between parentheses
 
-    //Step 2, evaluate expressions with * and / first (if they exist)
-    if (value.includes("/") || value.includes("*")){
-      let list = inputValue.split(/[+-]+/);
-      for (let item of list) {
-        //console.log(item);
-        value = value.replace(item, applyOperations(item))
-        console.log(`value: ${value}`)
+        console.log(`itemsInParen after split: ${itemsInParen}`); 
+        let parenAnswer = 0;
+        for (let item of itemsInParen) {
+          console.log(`item in parens: ${item}`)
+          item = item.replace(/[()]/,"");
+          parenAnswer = applyOperations(item);
+        }
+        
+        value = value.replace(/\(([^\)]+)\)/,parenAnswer)
+        //console.log(`itemsInParen: ${itemsInParen}`); 
       }
-    }
-   
-    value = applyOperations(value);
+
+      //Step 2, evaluate expressions with * and / first (if they exist)
+      while (value.includes("/") || value.includes("*")){
+        //replace the * and / expressions with the evaluated answers
+        value = value.replace( multDivExp, function(exp) {
+          return applyOperations(exp);
+        })
+        console.log(`value after calculating */ : ${value}`)
+      }
+
+      while (value.includes("+") || value.includes("-")){
+        //replace the * and / expressions with the evaluated answers
+        value = value.replace( addSubtExp, function(exp) {
+          return applyOperations(exp);
+        })
+        console.log(`value after calculating +- : ${value}`)
+      }
+    
+      if(value.includes("NaN") || value.includes("Syntax Error")) {
+        return setAnswerValue(`Syntax Error`);
+      } else {
+        value = applyOperations(value).toString();
+      }
+      //console.log(`value type: ${typeof(value)}`)
+    //} //for while loop
+
     return setAnswerValue(`Answer: ${value}`);
   }
 
   const applyOperations = (expression) => {
+
+    //check if number is negative
+    // if (expression[0] === "-" || expression[0] === "+") {
+    //   console.log(typeof(expression))
+    //   console.log("expression parsed: " + parseFloat(expression));
+    //   return parseFloat(expression);
+    // }
+
+    //checking for double operators
+    expression = expression.replace(/([+-])([+-])(\d|\.)/g, function (a, b, c, d) { 
+      return (b === c ? '+' : '-') + d; 
+    });
+
     let operator = expression.split(/[\d\s]+/).filter( op => { 
       return op !== "" && op !== "." && op !== "(" && op !== ")" 
     });
+
+    //constants can have +/- in front 
     let constants = expression.split(/[/+*-]+/); 
+
     console.log("expression: " + expression);
     console.log("operator: " + operator);
     console.log("constants: " + constants)
+    console.log(`constants[0]: ${constants[0]} constants[1]: ${constants[1]}`);
 
-    if (!operator || operator == "") {
-      return parseFloat(expression);
+    //for returning standalone "expressions"
+    if(!constants[1] || constants[1] === "") {
+      console.log(`returning parsed orig expression`)
+      return expression;
     }
-
+    
     switch(operator[0]) {
       case "/": 
         return parseFloat(constants[0]) / parseFloat(constants[1]);
@@ -87,6 +121,8 @@ export const InputBox = () => {
         return "Syntax Error";
     }
   }
+
+
 
 
   /************** EVENT HANDLERS ******************/
